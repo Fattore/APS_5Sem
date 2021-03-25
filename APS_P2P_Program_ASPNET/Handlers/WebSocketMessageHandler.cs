d7e5173eef644b6d9f6fb918696281b4edf8d1c3
Fratty <59932564+Fattore@users.noreply.github.com>
2021-03-25 00:25:38 -0300

    Add files via upload

---
+++ APS_P2P_Program_ASPNET/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using APS_P2P_Program_ASPNET.SocketsManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace APS_P2P_Program_ASPNET.Handlers
+{
+    public class WebSocketMessageHandler:SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connections) : base(connections)
+        {
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = Connections.GetId(socket);
+            await SendMessageToAll($"{socketId} just joined the party *****");
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = Connections.GetId(socket);
+            var message = $"{socketId} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await SendMessageToAll(message);
+        }
+    }
+}
---
+++ Console_APS/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Console_APS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Count() <= 2)
+            {
+                Process.Start("Console_APS.exe");
+            }
+
+            StartWebsockets().GetAwaiter().GetResult();
+        }
+
+        public static async Task StartWebsockets()
+        {
+            var client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri("ws://localhost:5000/ws"), CancellationToken.None);
+            Console.WriteLine($"web socket connection established @ {DateTime.UtcNow:F}");
+            var send = Task.Run(async () =>
+            {
+                string message;
+                while((message = Console.ReadLine()) != null && message != string.Empty)
+                {
+                    var bytes = Encoding.UTF8.GetBytes(message);
+                    await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
+                        CancellationToken.None);
+                }
+
+                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+            });
+            var receive = ReceiveAsync(client);
+            await Task.WhenAll(send, receive);
+        } 
+
+        public static async Task ReceiveAsync(ClientWebSocket client)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (true)
+            {
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, result.Count));
+                if(result.MessageType == WebSocketMessageType.Close)
+                {
+                    await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    break;
+                }
+            }
+        }
+    }
+}
